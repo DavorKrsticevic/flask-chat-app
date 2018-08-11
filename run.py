c0c18ba8e86c4e78762d65cc2901cd8b6f222e0e
Davor Krsticevic <davor.krsticevic@gmail.com>
2018-08-11 12:33:46 +0000

    Simple Long Polling With JavaScript

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect , render_template, request
 
 app = Flask(__name__)
 messages = []
@@ -7,24 +8,32 @@ messages = []
 
 def add_messages(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    massage_dict = {"timestamp": now , "from": username , "message" : message }
+    messages.append("({}) {}: {}".format(now, username, message))
 
 
 def get_all_messages():
     """Get al of the messages and separate them by a `br`"""
-    return "<br>".join(messages)
+    return messages
 
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")    
+        
 
 
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
+    messages = get_all_messages()
+    return render_template("chat.html", username=username , chat_messages = messages)
 
 
 @app.route('/<username>/<message>')
